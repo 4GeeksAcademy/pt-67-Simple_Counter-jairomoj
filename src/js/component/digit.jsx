4932af5ff01e3a6ee15d03b31133e3119465224f
jairomoj <58726090+jairomoj@users.noreply.github.com>
2024-05-28 17:12:40 +0000

    Primeras correcciones

--- src/js/component/digit.jsx
+++ src/js/component/digit.jsx
@@ -1,22 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Digit = (props) => {
-    const [seconds, setSeconds] = useState(0);
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setSeconds(seconds+1);
-        }, props.seconds);
-        // clearing interval
-        return () => clearInterval(timer);
-    });
-
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary text-bg-dark py-5">
             <div className="container-fluid">
                 <div className="card bg-secondary" style={{ width: "5rem" }}>
                     <div className="card-body">
-                        <h5 className="card-title text-center">{seconds % 10}</h5>
+                        <h5 className="card-title text-center">{props.seconds % 10}</h5>
                     </div>
                 </div>
             </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import Digit from "./digit";
@@ -6,15 +6,31 @@ import Watch from "./watch";
 
 //create your first component
 const Home = () => {
+	const [seconds, setSeconds] = useState(0);
+	let one = Math.floor(seconds / 1);
+	let two = Math.floor(seconds / 10);
+	let three = Math.floor(seconds / 100);
+	let four = Math.floor(seconds / 1000);
+	let five = Math.floor(seconds / 10000);
+	let six = Math.floor(seconds / 100000);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setSeconds(seconds + 1);
+		}, 1000);
+		// clearing interval
+		return () => clearInterval(timer);
+	});
+
 	return (
 		<div className="container-fluid d-flex justify-content-center">
 			<Watch />
-			<Digit seconds={100000000}/>
-			<Digit seconds={10000000}/>
-			<Digit seconds={1000000}/>
-			<Digit seconds={100000}/>
-			<Digit seconds={10000}/>
-			<Digit seconds={1000}/>
+			<Digit seconds={six} />
+			<Digit seconds={five} />
+			<Digit seconds={four} />
+			<Digit seconds={three} />
+			<Digit seconds={two} />
+			<Digit seconds={one} />
 		</div>
 	);
 };
